b76c2260a0665c16c6ef2e629f032957a1a06273
peter.prettenhofer <peter.prettenhofer@5472b044-fb82-11dd-8c4e-4bbb75a408b7>
2009-08-11 15:10:44 +0000

    Issue 1: virtualenv build fixed (patch by wojciech.malinowski)

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-
 from distutils.core import setup
 from distutils.extension import Extension
+import os.path
+
+import numpy
+numpy_path = os.path.join(numpy.__path__[0], 'core', 'include')
+
 setup(
     name = "rsvd",
-    ext_modules = [Extension("rsvd/rsvd", ["rsvd/rsvd.c"])],
+    ext_modules = [Extension("rsvd/rsvd", ["rsvd/rsvd.c"],
+                             include_dirs=[numpy_path],
+                             ),
+                  ],
     version = "0.1",
     description="A regularized SVD solver for partial matrices",
     author='Peter Prettenhofer',
     author_email='peter.prettenhofer@gmail.com',
     url="http://code.google.com/p/pyrsvd/",
     packages=['rsvd'],
-
 )
