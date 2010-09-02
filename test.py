39aa53f5ffa9d9ce58c378b90697eef8e43a20ef
peter.prettenhofer <peter.prettenhofer@5472b044-fb82-11dd-8c4e-4bbb75a408b7>
2010-09-02 21:55:48 +0000

    added learn curve script.

--- test.py
+++ test.py
@@ -1,26 +1,13 @@
-"""
-Test
-----
-This is a test
-"""
+#!/usr/bin/python
 
-#log# Automatic Logger file. *** THIS MUST BE THE FIRST LINE ***
-#log# DO NOT CHANGE THIS LINE OR THE TWO BELOW
-#log# opts = Struct({'__allownew': True, 'logfile': 'ipython_log.py'})
-#log# args = []
-#log# It is safe to make manual edits below here.
-#log#-----------------------------------------------------------------------
-import numpy as np
-from rsvd import RSVD
-print 'load ratings'
-ratings=np.load('data/ratings_float.arr')
-
-probeRatings=np.load('data/probe_ratings_float.arr')
+import sys
+import time
+print sys.argv[1]
+f = open(sys.argv[1],"r")
 
 
-model=RSVD.train(20,ratings,(17770,480189),probeRatings,100,randomize=False)
-print "model trained..."
+for line in f:
+    print line
+    time.sleep(1)
 
-model.save("models/t_20_001_011_100")
-
-
+f.close()
